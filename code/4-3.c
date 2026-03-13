ea5340e1e5d031736b69a42da3f7029e52856af5
wanderer-ing <jsjune1222@gmail.com>
2026-03-13 12:46:34 +0900

    Update 4-3.c

--- code/4-3.c
+++ code/4-3.c
@@ -2,6 +2,21 @@
 
 int main()
 {
+    int n;
+    scanf("%d", &n);
+    boolean b = false;
+
+    for(i=2; i<n; ++i){
+        if (n%i==0){
+            b = true;
+            break;
+        }
+    }
+
+    if(b){
+        printf("true");
+    } else{printf("false");}
+    
 
     return 0;
 }
