acff889d27f060d456aa3491049f7b8a207b0c28
wanderer-ing <jsjune1222@gamil.com>
2026-03-16 01:35:16 +0900

    last

--- code/4-3.c
+++ code/4-3.c
@@ -4,18 +4,23 @@ int main()
 {
     int n;
     scanf("%d", &n);
-    int b = 1;
-
-    for(int i=2; i<n; i++){
-        if (n%i==0){
-            b = 0;
-            break;
-        }
+    if(n==1){
+        print("false");
     }
+    else{
+        int b = 1;
 
-    if(b){
-        printf("true");
-    } else{printf("false");}
+        for(int i=2; i<n; i++){
+            if (n%i==0){
+                b = 0;
+                break;
+            }
+        }
+
+        if(b){
+            printf("true");
+        } else{printf("false");}
+    }
     
 
     return 0;
